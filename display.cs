291a97c50d1f5038ef7249943a943c6f1cb6dd6c
Ashratuz Zavin Asha <ashzavin@gmail.com>
2020-11-25 15:46:39 -0700

    Add files via upload

---
+++ display.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class display : MonoBehaviour
+{
+
+    public GameObject p;
+    public void Update()
+    {
+
+        GameObject surface = GameObject.Find("carsurface");
+        var videoPlayer = surface.AddComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.playOnAwake = false;
+        videoPlayer.url = "C:/Users/uCalgayPC/Desktop/old/Assets/videos/try.mp4";
+        videoPlayer.isLooping = true;
+        //Invoke("videoplay", 5f);
+
+        //Debug.Log(p.transform.position.z);
+
+        if (p.transform.position.z>=45.0)
+        {
+            //Debug.Log(p.transform.position.z);
+            videoPlayer.Play();
+        }
+        
+    }
+   
+}
