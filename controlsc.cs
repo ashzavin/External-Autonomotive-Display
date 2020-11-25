f8020f0e06e7c5b1eaec77ca4a0906707da6850b
Ashratuz Zavin Asha <ashzavin@gmail.com>
2020-11-25 15:20:39 -0700

    Add files via upload

---
+++ controlsc.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlsc : MonoBehaviour
+{
+    public GameObject p;
+    public GameObject q;
+    public GameObject way;
+    // Start is called before the first frame update
+    void awake()
+    {
+        //videoP = GetComponent<VideoPlayer>();
+    }
+    void Start()
+    {
+        p.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (way.transform.position.z >= 30.0)
+        {
+            p.SetActive(true);
+            q.SetActive(false);
+
+        }
+    }
+}
